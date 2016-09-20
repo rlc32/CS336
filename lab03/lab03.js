dca0b7f543e0ec1d39cd4290fd3a0142a555cfd0
rlc32 <rlc32@students.calvin.edu>
2016-09-20 00:29:19 -0400

    commiting lab03

---
+++ lab03/lab03.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+	res.send('Hello World!');
+
+});
+
+
+
+app.listen(5000, function () {
+	console.log('example app listening on port 5000!');app.use(express.static('public'));
+});
