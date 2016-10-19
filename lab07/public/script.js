40ed3045950c820d7eb6885f74d1720ba9ec8f3c
kvlinden <rlc32@students.calvin.edu>
2016-10-19 13:09:50 -0400

    adding final lab07

--- lab07/public/script.js
+++ lab07/public/script.js
@@ -1,19 +1,28 @@
 $( document ).ready(function() {
- 
-    $( "a" ).click(function( event ) {
- 
-        alert( "As you can see, the link no longer took you to jquery.com" );
- 
-        event.preventDefault();
- 
-    });
-    
-    $( "a" ).addClass( "test" );
-    $(document.createElement('div'));
-    var myNewElement = $( "<p>no data yet.........</p>" );
- 
-    myNewElement.insertAfter( "#get-data-button" );
- 
+ 	//click event for the jquery button widget.
+    $( "#get-data-button" ).click(function( event ) {
+    	$.ajax({
+    		url:"/hello",
+    		type:"GET",
+    		data: {name: "lab07"}
+    	})
+    	//this function activates upon a seccesful return of data. adds the data that was returned to the screen in a nice message
+    	.done(function(json_string){
+    		const json = JSON.parse(json_string);
+    		$("body").append("<p>" + json.message +"<p>");
+    		console.log('request was completed');
+
+    	})
+    	//error message alert when the get data fails
+    	.fail(function( xhr, status, error){
+    		alert("There was an error");
+    		console.log("Error: " + error);
+    		console.log("Status: " + status);
+    		console.dir( xhr);
+    	})
+    	
+})
+
    
 });
   
